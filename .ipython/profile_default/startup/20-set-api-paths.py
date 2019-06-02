3058dc4bd50ec3cc17901dbb08f72477aee6462a
Dinis Cruz <dinis.cruz@owasp.org>
2019-06-02 12:46:42 +0100

    Create 20-set-api-paths.py

---
+++ .ipython/profile_default/startup/20-set-api-paths.py
@@ -0,0 +1,9 @@
+import sys ;
+sys.path.append('./api')
+sys.path.append('../api')
+sys.path.append('../../api')
+sys.path.append('../../../api')
+sys.path.append('../../../../api')
+
+from api.utils import *
+import pandas as pd
