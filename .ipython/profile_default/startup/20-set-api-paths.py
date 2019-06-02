13c2bcf5a9112b99df890774e1f5c6daf6c35af9
Dinis Cruz <dinis.cruz@owasp.org>
2019-06-02 18:27:30 +0100

    Update 20-set-api-paths.py

--- .ipython/profile_default/startup/20-set-api-paths.py
+++ .ipython/profile_default/startup/20-set-api-paths.py
@@ -4,6 +4,10 @@ sys.path.append('../api')
 sys.path.append('../../api')
 sys.path.append('../../../api')
 sys.path.append('../../../../api')
+sys.path.append('../oss2019/notebooks/api')
+sys.path.append('../../oss2019/notebooks/api')
+sys.path.append('../../../oss2019/notebooks/api')
+sys.path.append('../../../../oss2019/notebooks/api')
 
 from api.utils import *
 import pandas as pd
